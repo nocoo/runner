4b04503f1cf2aa9b34183ac05cf6033c7043d1f8
Zheng Li <lizheng@lizheng.me>
2026-02-04 07:56:06 +0800

    chore: add GRDB dependency for SQLite storage

--- runner-swift/Package.swift
+++ runner-swift/Package.swift
@@ -5,14 +5,16 @@ let package = Package(
     name: "runner",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0")
     ],
     targets: [
         // Library containing all core logic (testable)
         .target(
             name: "RunnerLib",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "GRDB", package: "GRDB.swift")
             ],
             path: "Sources/RunnerLib"
         ),
